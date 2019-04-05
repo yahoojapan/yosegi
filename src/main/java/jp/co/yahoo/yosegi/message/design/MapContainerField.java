91bd15fde893293168b5d40f55bc01b1db8917c5
Yasunori Ohto <yoto@yahoo-corp.jp>
2019-04-05 10:27:02 +0900

    remove unwanted class

--- src/main/java/jp/co/yahoo/yosegi/message/design/MapContainerField.java
+++ src/main/java/jp/co/yahoo/yosegi/message/design/MapContainerField.java
@@ -26,7 +26,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.stream.Collectors;
 
 public class MapContainerField extends SimpleField implements INamedContainerField {
   private final IField defaultField;
