3d085c78439526fa3ef8086aa73ab4cc92c7398b
Koji <kijima@yahoo-corp.jp>
2019-04-21 00:27:32 +0900

    [Yosegi-33] Create Docker image to build

--- src/main/java/jp/co/yahoo/yosegi/reader/YosegiSchemaFileReader.java
+++ src/main/java/jp/co/yahoo/yosegi/reader/YosegiSchemaFileReader.java
@@ -27,7 +27,6 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import javax.xml.ws.WebServiceException;
 
 public class YosegiSchemaFileReader implements Closeable,IStreamReader {
 
